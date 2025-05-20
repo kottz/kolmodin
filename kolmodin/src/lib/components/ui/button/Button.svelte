2f3dccbbcf947d0a6d8f982205c6ab4f5ea11d2b
Edward Källstedt <edward.kallstedt@gmail.com>
2025-05-20 05:53:50 +0200

    add button component

---
+++ kolmodin/src/lib/components/ui/button/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import { type Size, type Variant, buttonVariants } from './index.js';
+	import { cn } from '$lib/utils/cn.js';
+
+	let {
+		class: className,
+		variant = 'default',
+		size = 'default',
+		children,
+		...restProps
+	}: HTMLButtonAttributes & { variant?: Variant; size?: Size } = $props();
+</script>
+
+<button class={cn(buttonVariants({ variant, size }), className)} {...restProps}>
+	{@render children?.()}
+</button>
